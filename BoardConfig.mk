80bfdfa8eaf4157f0734c28392c4571005fcd8d2
ananjaser1211 <ananjaser@gmail.com>
2018-01-08 03:25:15 +0400

    Update boardconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,12 +1,18 @@
+USE_CAMERA_STUB := true
 DEVICE_TREE := device/samsung/treltexx
 
-# Bootloader
+# Platform
+TARGET_SOC := exynos5433
+BOARD_VENDOR := samsung
+TARGET_BOARD_PLATFORM := exynos5
+TARGET_BOARD_PLATFORM_GPU := mali-t760mp6
 TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := universal5433
 
-# Platform
-TARGET_BOARD_PLATFORM := exynos5
-TARGET_BOARD_PLATFORM_GPU := mali-t760mp6
+# Flags
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60 -DQCOM_HARDWARE
 
 # Architecture
 TARGET_ARCH := arm
@@ -18,17 +24,14 @@ TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/samsung/trelte
+TARGET_PREBUILT_KERNEL := device/samsung/treltexx/Image
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_HEADER_ARCH := arm
-TARGET_KERNEL_CONFIG := trelte_00_defconfig
-TARGET_KERNEL_VARIANT_CONFIG := 
-KERNEL_TOOLCHAIN_PREFIX := arm-linux-androideabi-
 
 # DTB
 TARGET_DTBH_PLATFORM_CODE := 0x000050a6
 TARGET_DTBH_SUBTYPE_CODE  := 0x217584da
-TARGET_PREBUILT_DTB := $(DEVICE_TREE)/dtb.img
+TARGET_PREBUILT_DTB := device/samsung/treltexx/dt.img
 
 # Boot image
 BOARD_KERNEL_CMDLINE := # Exynos doesn't take cmdline arguments from boot image
@@ -36,7 +39,6 @@ BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 # 000RU = recovery kernel, 000KU = system kernel
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SYSMAGIC000RU
-BOARD_CUSTOM_BOOTIMG_MK :=  $(DEVICE_TREE)/bootimg.mk
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x000E00000
@@ -55,15 +57,24 @@ TARGET_USERIMAGES_USE_F2FS := true
 
 # TWRP specific build flags
 TW_THEME := portrait_hdpi
+TW_HAS_MTP := true
 RECOVERY_SDCARD_ON_DATA := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/15400000.usb/15400000.dwc3/gadget/lun%d/file"
 TW_BRIGHTNESS_PATH := "/sys/devices/13800000.decon_fb/backlight/panel/brightness"
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 162
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
+TW_MTP_DEVICE := "/dev/mtp_usb"
+TW_EXCLUDE_SUPERSU := true
 TW_INCLUDE_NTFS_3G := true
+TW_EXTRA_LANGUAGES := true
+TW_USE_NEW_MINADBD := true
+TWRP_INCLUDE_LOGCAT := true
+
+# Color fix
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
 # exFAT drivers included in the kernel
 TW_NO_EXFAT_FUSE := true
@@ -73,3 +84,8 @@ TW_EXCLUDE_SUPERSU := true
 
 # Encryption support
 TW_INCLUDE_CRYPTO := true
+
+# Debug flags
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+TWRP_EVENT_LOGGING := false
